8a835850b3429d92bb236772dc43b3882a31848c
ltjxwxz <1850347685@qq.com>
2017-11-06 11:25:38 +0800

    没改什么

--- spring-boot-hello/src/main/java/com/didispace/web/HelloController.java
+++ spring-boot-hello/src/main/java/com/didispace/web/HelloController.java
@@ -1,11 +1,12 @@
 package com.didispace.web;
 
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+	
 	@RequestMapping("/hello")
 	public String index() {
 		return "Hello World";
