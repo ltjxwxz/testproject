37474c1bc60b3e52865396e058cc1deecf166f11
ltjxwxz <1850347685@qq.com>
2017-11-06 18:13:12 +0800

    重新导包

--- spring-boot-hello/src/main/java/com/didispace/web/HelloController.java
+++ spring-boot-hello/src/main/java/com/didispace/web/HelloController.java
@@ -1,6 +1,5 @@
 package com.didispace.web;
 
-import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
